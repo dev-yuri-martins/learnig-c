852f56f146c8f7d29ea648b5c78a4f97a3637096
Yuri-silva-dev <yurimartins.dev@yahoo.com>
2026-03-14 15:07:49 -0300

    beecrowd_1005: average 1

---
+++ beecrowd1005.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+   float a, b, media;
+   
+   scanf(" %f %f", &a, &b);
+   a = a * 3.5;
+   b = b * 7.5;
+   media = (a + b) / 11;
+   printf("MEDIA = %.5f\n", media);
+      
+   return 0;
+}
