2bd1c4ae72492a45387d83721f7a392909ff689a
Yuri-silva-dev <yurimartins.dev@yahoo.com>
2026-05-12 11:49:59 -0300

    beecrowd_1075: remaining 2

---
+++ beecrowd1075.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int x; 
+    scanf(" %d", &x);
+    
+    for(int i = 1; i < 10000; i ++) {
+        if (i % x == 2) {
+            printf("%d\n", i);
+        } else {
+            //nadad acontece
+        }
+    }
+    return 0;
+}
